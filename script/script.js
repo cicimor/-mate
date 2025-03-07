397f536564040301bb42dea481e10bbf6ef7617c
Artem Rizakhanov <107078736+cicimor@users.noreply.github.com>
2025-03-08 01:29:29 +0300

    uppdate

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+
+
+
 butt.onclick = function() {
     var val_Tp = document.getElementById('input-1').value;
     var val_Yp = document.getElementById('input-2').value;
@@ -25,7 +28,6 @@ async function processData(pName,tokenName) {
         const sum = token.reduce((acc, value) => acc + value, 0);  // Суммируем все элементы массива
         const average = sum / token.length;  // Делим сумму на количество элементов
         document.getElementById(`${pName}`).innerHTML = average.toFixed(4);
-
 }
 
 // Функция для обработки данных по токену
@@ -36,7 +38,8 @@ function processTokenData(data, tokenName) {
     return reward
 }
 
-// Вызов основной функции
+
+
 
 processData("CATI-p","CATI");
 processData("TON-p","TON");
@@ -46,3 +49,44 @@ processData("DOGS-p","DOGS");
 processData("PX-p","PX");
 processData("MAJOR-p","MAJOR");
 processData("MATE-p","MATE");
+
+
+async function processDataPrice(pNamePrice,tokenName, tokenN) {
+    
+    const data = await loadJson();  // Дожидаемся завершения загрузки
+        let token = processTokenData(data, `${tokenName}`);
+        const sum = token.reduce((acc, value) => acc + value, 0);  // Суммируем все элементы массива
+        const average = sum / token.length;  // Делим сумму на количество элементов
+        
+
+
+
+
+        let url = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenN}&vs_currencies=usd`;
+    
+    try {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const response = await axios.get(url);
+        const price = response.data[tokenN]?.usd;
+
+        if (!price) {
+            document.getElementById(`${pNamePrice}`).innerText = "Ошибка: токен не найден!";
+            return;
+        }
+
+        document.getElementById(`${pNamePrice}`).innerText = ` $${(1 * price).toFixed(6)} USD`;
+    } catch (error) {
+        document.getElementById(`${pNamePrice}`).innerText = "Ошибка при получении данных!";
+        console.error(error);
+    }
+}
+
+
+processDataPrice("CATI-pPrice","CATI","catizen")
+processDataPrice("TON-pPrice","TON","the-open-network")
+processDataPrice("NOT-pPrice","NOT","notcoin")
+processDataPrice("DOGS-pPrice","DOGS","dogechain")
+processDataPrice("PX-pPrice","PX","not-pixel")
+processDataPrice("MAJOR-pPrice","MAJOR","major")
+
+
