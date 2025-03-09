687595ae62a0d570069a5caf9fcecbab86ca49ee
Artem Rizakhanov <107078736+cicimor@users.noreply.github.com>
2025-03-10 01:09:01 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ butt.onclick = function () {
     var val_Tp = document.getElementById('input-1').value;
     var val_Yp = document.getElementById('input-2').value;
     var val_Br = document.getElementById('input-3').value;
-    var result = (val_Br * 0.5) * (val_Yp / val_Tp);
+    var result = (val_Br * 0.7) * (val_Yp / val_Tp);
     document.getElementById('str').innerHTML = result.toFixed(4) + ' TOKENS';
 };
 
